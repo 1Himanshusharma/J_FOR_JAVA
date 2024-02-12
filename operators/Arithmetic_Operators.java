879b4974c805d36fbdf1eb42e1312f73c4d88488
1Himanshusharma <himanshusharma272002@gmail.com>
2024-02-12 11:38:38 +0530

    .

---
+++ operators/Arithmetic_Operators.java
@@ -0,0 +1,12 @@
+class Arithmetic_Operators{
+
+  public static void main(String[] args){
+    int a = 14,b = 5;
+
+    System.out.println("the addition a + b: " + (a + b));
+    System.out.println("the subtraction a - b: " + (a - b));
+    System.out.println("the Division a / b: " + (a / b));
+    System.out.println("the Multiplication a * b: " + (a * b));
+     
+  }
+}
---
+++ operators/unary.java
@@ -0,0 +1,9 @@
+public class unary {
+  public static void main(String[] args){
+    int a = 4,b = 5;
+    System.out.println("a++ : " + a++);
+    System.out.println("--a : " + --a);
+    System.out.println("b++ : " + b++);
+    System.out.println("--b : " + --b);
+  }
+}
